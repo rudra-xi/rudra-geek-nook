c5e7ed38971231189d7a641e645dc857f5c4bc1b
rudra <goutam.prasad.2707@gmail.com>
2025-11-27 23:15:51 +0530

    feat: Add UI components using Shadcn UI
    This commit introduces a set of UI components built with Shadcn UI. The
    following components have been added:
    
    - Accordion - Button - Dialog - InputGroup - Input - Label - Textarea -
    Toaster - AnimatedCursor
    
    These components provide a foundation for building the UI.

---
+++ src/components/ui/index.ts
@@ -0,0 +1,59 @@
+// shadcn ui
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@radix-ui/react-accordion";
+
+import { Button } from "./button";
+
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupButton,
+	InputGroupInput,
+	InputGroupText,
+	InputGroupTextarea,
+} from "./input-group";
+
+import { Label } from "./label";
+import { Toaster, toast } from "sonner";
+
+import { CursorFollow, CursorProvider } from "./shadcn-io/animated-cursor";
+
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogFooter,
+	DialogTrigger,
+} from "./dialog";
+
+export {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+	Button,
+	CursorFollow,
+	CursorProvider,
+	InputGroup,
+	InputGroupAddon,
+	InputGroupButton,
+	InputGroupInput,
+	InputGroupText,
+	InputGroupTextarea,
+	Label,
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogFooter,
+	DialogTrigger,
+	Toaster,
+	toast,
+};

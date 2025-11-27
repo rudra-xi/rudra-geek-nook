b89c69eb69f0351a90619c00058cbd9461d8df6d
rudra <goutam.prasad.2707@gmail.com>
2025-11-27 23:13:15 +0530

    feat: Add assets and resume

---
+++ public/index.ts
@@ -0,0 +1,7 @@
+import logo from "./assets/logo.svg";
+import logo_light from "./assets/logo_light.svg";
+import resumePreview from "./assets/resume-preview.png";
+
+const resume = "./assets/resume.pdf";
+
+export { logo, logo_light, resumePreview, resume };

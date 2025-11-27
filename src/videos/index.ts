2301297d4fcf0e0dfdff2122f55691a3452502ce
rudra <goutam.prasad.2707@gmail.com>
2025-11-27 23:15:24 +0530

    feat: Add video metadata and index file
    Add JSON metadata for six videos and create an index file to export
    them.

---
+++ src/videos/index.ts
@@ -0,0 +1,15 @@
+import amIHerePreview from "./am-i-here-or-not.mp4.json";
+import archivePreview from "./archive-dark-desire.mp4.json";
+import financePreview from "./finance-flow.mp4.json";
+import flickerPreview from "./flicker-bar-site.mp4.json";
+import bezierPreview from "./quick-bezier.mp4.json";
+import sylvanPreview from "./sylvan-reveal-animation.mp4.json";
+
+export {
+    amIHerePreview,
+    flickerPreview,
+    bezierPreview,
+    sylvanPreview,
+    archivePreview,
+    financePreview,
+};
